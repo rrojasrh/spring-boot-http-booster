23a3237e1d42fa6d745c83c65c3ed8000acbe704
Raúl Rojas @ RHCRW <rrojas@redhat.com>
2019-04-30 01:58:49 +0000

    Se ha modificado el servicio

--- src/main/java/io/openshift/booster/service/Greeting.java
+++ src/main/java/io/openshift/booster/service/Greeting.java
@@ -18,7 +18,7 @@ package io.openshift.booster.service;
 // tag::snippet-greeting[]
 public class Greeting {
 
-    public static final String FORMAT = "Hello, %s!";
+    public static final String FORMAT = "¡Saludos, %s!";
 
     private final String content;
 
